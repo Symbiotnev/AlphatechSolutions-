ff4d3ff017974456cfe3627be4f089a3b02cb7e6
root <root@DESKTOP-QVMAUD2>
2024-06-18 12:54:17 +0300

    new

--- script.js
+++ script.js
@@ -107,3 +107,21 @@ scrolltop.forEach((el)=>observer.observe(el));
 /*Contact Input */
 
         
+const header = document.querySelector("header");
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
